9a2e022ae549126df86c8269eb0264fb0080d9cf
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2022-09-26 21:17:57 -0400

    initial commit

---
+++ src/cam_detector.py
@@ -0,0 +1,53 @@
+import cv2
+from pupil_apriltags import Detector
+
+vid = cv2.VideoCapture(0)
+
+while True:
+    # load the input image and convert it to grayscale
+    ret, image = vid.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # define the AprilTags detector options and then detect the AprilTags
+    # in the input image
+    at_detector = Detector(families='tag36h11',
+                           nthreads=1,
+                           quad_decimate=1.0,
+                           quad_sigma=0.0,
+                           refine_edges=1,
+                           decode_sharpening=0.25,
+                           debug=0)
+
+    results = at_detector.detect(gray, estimate_tag_pose=False, camera_params=None, tag_size=None)
+
+    # loop over the AprilTag detection results
+    for r in results:
+        # extract the bounding box (x, y)-coordinates for the AprilTag
+        # and convert each of the (x, y)-coordinate pairs to integers
+        (ptA, ptB, ptC, ptD) = r.corners
+        ptB = (int(ptB[0]), int(ptB[1]))
+        ptC = (int(ptC[0]), int(ptC[1]))
+        ptD = (int(ptD[0]), int(ptD[1]))
+        ptA = (int(ptA[0]), int(ptA[1]))
+        # draw the bounding box of the AprilTag detection
+        cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+        cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+        cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+        cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+        # draw the center (x, y)-coordinates of the AprilTag
+        (cX, cY) = (int(r.center[0]), int(r.center[1]))
+        cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+        # draw the tag family on the image
+        tagFamily = r.tag_family.decode("utf-8")
+        cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    # show the output image after AprilTag detection
+
+    cv2.imshow('frame', image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
